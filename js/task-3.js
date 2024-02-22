3074e2a55e78278bf4940dc77aabba98b8f8e2b6
Mark-goga <markgala06@gmail.com>
2024-02-22 17:56:39 +0200

    strat

--- js/task-3.js
+++ js/task-3.js
@@ -3,7 +3,7 @@ function getElementWidth(content, padding, border) {
     padding = Number.parseFloat(padding) * 2;
     border = Number.parseFloat(border) * 2;
     const totalWidth = content + padding + border;
-    console.log(totalWidth);
+    return totalWidth;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
